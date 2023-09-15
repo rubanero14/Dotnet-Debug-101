d914a9ce90195f3430418ab69bc493ef0c3dec54
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-09-15 23:38:44 +0800

    stash

--- Debug-101/Program.cs
+++ Debug-101/Program.cs
@@ -8,7 +8,7 @@ greeting messages
     - Once fixed is done, press stop debug process
     - Then in cli, type `dotnet clean' to delete the existing build files
     - Again in cli, `type dotnet build` to rebuild new build files based on the changes made (without these, the changes wont reflect and will keep showing previous code remnants)
-    - You can start to debug now if you want by pressing Start Debugging in Run menu 
+    - You can start to debug now if you want by pressing Start ebugging in Run menu 
 */
 
 string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
