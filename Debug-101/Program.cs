5c1d9c76f41a70de7e77997ef4053a8e40e56ea9
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-09-15 14:57:57 +0800

    wip

--- Debug-101/Program.cs
+++ Debug-101/Program.cs
@@ -3,6 +3,14 @@ This code uses a names array and corresponding methods to display
 greeting messages
 */
 
+/*
+    ATTENTION!:
+    - Once fixed is done, press stop debug process
+    - Then in cli, type `dotnet clean' to delete the existing build files
+    - Again in cli, `type dotnet build` to rebuild new build files based on the changes made (without these, the changes wont reflect and will keep showing previous code remnants)
+    - You can start to debug now if you want by pressing Start Debugging in Run menu 
+*/
+
 string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
 
 string messageText = "";
@@ -14,8 +22,7 @@ foreach (string name in names)
     else if (name == "Andrew")
         messageText = AndrewMessage();
     else if (name == "AllGreetings")
-        messageText = SophiaMessage();
-    messageText = messageText + "\n\r" + AndrewMessage();
+        messageText = SophiaMessage() + "\n\r" + AndrewMessage();
 
     Console.WriteLine(messageText + "\n\r");
 }
