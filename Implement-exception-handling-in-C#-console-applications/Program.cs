138d238d047e105aafe9150e675e212c0b729425
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-09-16 16:45:05 +0800

    wip

---
+++ Implement-exception-handling-in-C#-console-applications/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("============================================================================================");
+// Implement exception handling in C# console applications
+
+// Examine exceptions and the exception handling process
+// Exception handling keywords, code blocks, and patterns
+try
+{
+    // try code block - code that may generate an exception
+}
+catch
+{
+    // catch code block - code to handle an exception
+}
+finally
+{
+    // finally code block - code to clean up resources
+}
+
+Console.WriteLine("============================================================================================");
