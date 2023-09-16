0f8140a34d071346bdcbc13f5cff39fa8e4853e6
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-09-16 16:54:05 +0800

    Examine exceptions and the exception handling process

--- Implement-exception-handling-in-C#-console-applications/Program.cs
+++ Implement-exception-handling-in-C#-console-applications/Program.cs
@@ -17,3 +17,36 @@ finally
 }
 
 Console.WriteLine("============================================================================================");
+
+// Exception handling process
+try
+{
+    // Step 1: code execution begins
+    try
+    {
+        // Step 2: an exception occurs here
+    }
+    finally
+    {
+        // Step 4: the system executes the finally code block associated with the try statement where the exception occurred
+    }
+
+}
+catch // Step 3: the system finds a catch clause that can handle the exception
+{
+    // Step 5: the system transfers control to the first line of the catch code block
+}
+
+/*
+    In this example as above, the following process occurs:
+
+    Execution begins in the code block of the outer try statement.
+    An exception is thrown in the code block of the inner try statement.
+    The runtime finds the catch clause associated with the outer try statement.
+    Before the runtime transfers control to the first line of the catch code block, it executes the finally clause associated with the inner try statement.
+    The runtime then transfers control to the first line of the catch code block and executes the code that handles the exception.
+*/
+
+Console.WriteLine("============================================================================================");
+
+// More on Exceptions and Exception Handling - https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/exceptions/
