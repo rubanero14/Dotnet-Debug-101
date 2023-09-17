0a752367ecbae85e33ca08149370fb831d61b8f6
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-09-17 16:07:46 +0800

    Exercise - Catch specific exception types

--- Exercise-Implement-try-catch-exception-handling/Program.cs
+++ Exercise-Implement-try-catch-exception-handling/Program.cs
@@ -188,5 +188,27 @@ static void WriteMessage2()
 */
 Console.WriteLine("============================================================================================");
 // Catch separate exception types in a code block
+// inputValues is used to store numeric values entered by a user
+string[] inputValues = new string[] { "three", "9999999999", "0", "2" };
 
+foreach (string inputValue in inputValues)
+{
+    int numValue = 0;
+    try
+    {
+        numValue = int.Parse(inputValue);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid readResult. Please enter a valid number.");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("The number you entered is too large or too small.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
 Console.WriteLine("============================================================================================");
