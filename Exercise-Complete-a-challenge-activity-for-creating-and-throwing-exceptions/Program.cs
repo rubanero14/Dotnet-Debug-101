611e0d6e2cd4511bcdfe70276b54e4833ae04313
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-09-18 19:04:55 +0800

    Review the solution for the create and throw exceptions challenge

---
+++ Exercise-Complete-a-challenge-activity-for-creating-and-throwing-exceptions/Program.cs
@@ -0,0 +1,79 @@
+﻿Console.WriteLine("============================================================================================");
+// Exercise - Complete a challenge activity for creating and throwing exceptions
+// Create and throw exceptions challenge
+string[][] userEnteredValues = new string[][]
+{
+            new string[] { "1", "2", "3"},
+            new string[] { "1", "two", "3"},
+            new string[] { "0", "1", "2"}
+};
+
+try
+{
+    Workflow1(userEnteredValues);
+    Console.WriteLine("'Workflow1' completed successfully.");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("An error occurred during 'Workflow1'.");
+    Console.WriteLine(ex.Message);
+}
+
+static void Workflow1(string[][] userEnteredValues)
+{
+    foreach (string[] userEntries in userEnteredValues)
+    {
+        try
+        {
+            Process1(userEntries);
+            Console.WriteLine("'Process1' completed successfully.");
+            Console.WriteLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("'Process1' encountered an issue, process aborted.");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
+        }
+    }
+}
+
+static void Process1(String[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        bool integerFormat = int.TryParse(userValue, out valueEntered);
+
+        if (integerFormat == true)
+        {
+            if (valueEntered != 0)
+            {
+                checked
+                {
+                    int calculatedValue = 4 / valueEntered;
+                }
+            }
+            else
+            {
+                throw new DivideByZeroException("Invalid data. User input values must be non-zero values.");
+            }
+        }
+        else
+        {
+            throw new FormatException("Invalid data. User input values must be valid integers.");
+        }
+    }
+}
+Console.WriteLine("============================================================================================");
+/*
+    Reference materials:
+    - You can find additional information about using exceptions here: https://learn.microsoft.com/dotnet/csharp/fundamentals/exceptions/using-exceptio ns.
+    - You can find additional information about creating and throwing here: https://learn.microsoft.com/dotnet/csharp/fundamentals/exceptions/creating-and-throwing-excepti on.
+    - You can find additional information about creating user defined exceptions here: https://learn.microsoft.com/dotnet/standard/exceptions/how-to-create-user-defined-exceptio   ns.
+    - You can find additional information about localized exception messages here: https://learn.microsoft.com/dotnet/standard/exceptions/how-to-create-localized-exception-messag  es.
+    - You can find additional information about explicitly throwing exceptions here: https://learn.microsoft.com/dotnet/standard/exceptions/how-to-explicitly-throw-exceptio    ns.
+    - You can find additional information about using specific exceptions here: https://learn.microsoft.com/dotnet/standard/exceptions/how-to-use-specific-exceptions-in-a-catch-block.
+*/
+
